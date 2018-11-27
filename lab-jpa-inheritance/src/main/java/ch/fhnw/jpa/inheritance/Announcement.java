4571125d4bca5c98cc33d01443a275ae1037bc04
Carlo Baumann <carlo.baumann@students.fhnw.ch>
2018-11-27 10:25:19 +0100

    Lektion 6: AB JPA

---
+++ lab-jpa-inheritance/src/main/java/ch/fhnw/jpa/inheritance/Announcement.java
@@ -0,0 +1,22 @@
+package ch.fhnw.jpa.inheritance;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Announcement extends Topic {
+	private LocalDate validUntil;
+	
+	protected Announcement() {}
+	
+	public Announcement(String title, String owner) {
+		this(title, owner, null);
+	}
+
+	public Announcement(String title, String owner, LocalDate validUntil) {
+		super(title, owner);
+		this.validUntil = validUntil;
+	}
+
+}
